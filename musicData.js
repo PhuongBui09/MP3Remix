0b16fe5d2d39283185c9b55d22180e914073826d
PhuongBui09 <phuongkaratedo113@gmail.com>
2024-09-02 09:21:12 +0700

    first commit

---
+++ musicData.js
@@ -0,0 +1,47 @@
+const musicList = [
+    {
+      id: 1,
+      name: "徐俊雅《爱你",
+      singer: "1.1加速版 抖音版",
+      linkAu: "爱你.mp3",
+      img: "./assests/img/爱你.jpg",
+    },
+    {
+      id: 2,
+      name: "Không thể độ luân hồi",
+      singer: "Doãn Tích Miên",
+      linkAu: "Không thể độ luân hồi.mp3",
+      img: "./assests/img/Không thể độ luân hồi.jpg",
+    },
+    {
+        id: 3,
+        name: "Lao tâm khổ tứ",
+        singer: "Thanh Hưng - Huy PT Remix",
+        linkAu: "Lao Tâm Khổ Tứ.mp3",
+        img: "./assests/img/Lao Tâm Khổ Tứ.jpg",
+      },
+      {
+        id: 4,
+        name: "Hứa đợi nhưng chẳng tới",
+        singer: "LÂM TUẤN x TLEE REMIX",
+        linkAu: "Hứa đợi nhưng chẳng tới.mp3",
+        img: "./assests/img/Hứa đợi nhưng chẳng tới.jpg",
+      },
+      {
+        id: 5,
+        name: "Yêu em nhưng không với tới x Vở kịch của em",
+        singer: "DC Tâm x SS x AM Remix",
+        linkAu: "Yêu em nhưng không với tới x Vở kịch của em.mp3",
+        img: "./assests/img/Yêu em nhưng không với tới x Vở kịch của em.jpg",
+      },
+      {
+        id: 6,
+        name: "Em nào có tội",
+        singer: "Thương Võ - Tipo ft. Ciray Remix",
+        linkAu: "Em nào có tội.mp3",
+        img: "./assests/img/Em nào có tội.jpg",
+      },
+  ];
+  
+  export { musicList };
+  
